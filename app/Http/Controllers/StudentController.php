4948957fbbf01a2d18d6f384ea51b50f6d6f6c15
jasonx <caranzojason@gmail.com>
2020-11-30 21:42:46 +0800

    update

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -57,6 +57,49 @@ class StudentController extends Controller
         }
     }
 
+    public function updateStudent(Request $request)
+    {
+
+        try {
+            $stud = Student::find( $request->id);
+            $stud->studentno =  $request->studentno;
+            $stud->firstname =  $request->firstname;
+            $stud->middlename =  $request->middlename;
+            $stud->lastname =  $request->lastname;
+            $stud->email =  $request->email;
+            $stud->grade =  $request->grade;
+            $stud->department =  $request->department;
+            $stud->strand =  $request->strand;
+            $strDate = $request->dob['year'] . "/" . $request->dob['month'] .'/'. $request->dob['day'];
+            $stud->dob =  $strDate;
+            $stud->place_of_birth =  $request->place_of_birth;
+            $stud->contactno =  $request->contactno;
+            $stud->address =  $request->address;
+            $stud->nationality =  $request->nationality;
+            $stud->age =  $request->age;
+            $stud->gender =  $request->gender;
+            $stud->religion =  $request->religion;
+            $stud->fathername =  $request->fathername;
+            $stud->fatherocc =  $request->fatherocc;
+            $stud->fathercontact =  $request->fathercontact;
+            $stud->fatherplace =  $request->fatherplace;
+            $stud->mothername =  $request->mothername;
+            $stud->motherocc =  $request->motherocc;
+            $stud->mothercontact =  $request->mothercontact;
+            $stud->motherplace =  $request->motherplace;
+            $stud->guardian_name =  $request->guardian_name;
+            $stud->guardian_contactno =  $request->guardian_contactno;
+            $stud->guardian_relation =  $request->guardian_relation;
+            $stud->status =  "A";//Open
+            $stud->semester =  $request->semester;
+            $stud->save();
+            
+            return response()->json($stud , 201);
+        } catch (Exception $e) {
+            throw new HttpException(200, $e->getMessage());
+        }
+    }
+
     public function studentlist($page = 0,$pageSize = 0,$searchField = "")
     {
             $stud = (object) [
